a2c6574615c818e59ef572b19ed5aaf1fa62f128
mikeilab21 <miguel@ilab.digital>
2024-08-24 11:07:45 -0500

    ADD: Creación componente de detalle

--- AppProducts/src/app/components/product/product.module.ts
+++ AppProducts/src/app/components/product/product.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductDetailComponent
+  ],
   imports: [
     CommonModule,
     ProductRoutingModule,
