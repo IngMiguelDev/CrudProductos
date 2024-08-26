76e49e821d6803003f176a2fe73afdbd376833c0
mikeilab21 <miguel@ilab.digital>
2024-08-25 19:41:40 -0500

    ADD: Visualizacion de detalle del producto

--- AppProducts/src/app/components/product/product-detail/product-detail.component.ts
+++ AppProducts/src/app/components/product/product-detail/product-detail.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit  {
 
+  products!: any;
+  id!: number; 
+
+  constructor(
+    private _productService: ProductService,
+    private router: Router,
+    private activeRoute: ActivatedRoute
+  ) { 
+    
+    this.id = Number(this.activeRoute.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+    this.getProductId(this.id);
+  }
+
+  getProductId(id: number){
+    this._productService.getProductById(id).subscribe({next: producto=>{
+      console.log('data',  producto);
+      this.products = producto;
+    }
+   });
+  }
+
+  goBack(){
+    this.router.navigate(['/product/lista-productos']);
+  }
 }
