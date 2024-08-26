3c316e5515654b7c40b309eb19a78ded6f081263
mikeilab21 <miguel@ilab.digital>
2024-08-25 22:59:13 -0500

    ADD: Se añadio documentacion

--- AppProducts/src/app/app-routing.module.ts
+++ AppProducts/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'product', loadChildren: () => import('./components/product/product.module').then(m => m.ProductModule) },
   { path: 'home', loadChildren: () => import('./components/presentation/presentation.module').then(m => m.PresentationModule) },
+  { path: 'documentation', loadChildren: () => import('./components/documentation/documentation.module').then(m => m.DocumentationModule) },
   { path: '**', redirectTo: '/home'}
 ];
 
